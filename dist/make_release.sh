4862e1dd9aad6887d1678909e3d923e3418743bb
ww <winslow.williams>
2017-11-25 06:09:20 -0500

    updated submodules, added colscript to dist

--- dist/make_release.sh
+++ dist/make_release.sh
@@ -5,12 +5,14 @@ if [ 1 == $# ]; then
     mkdir bqcsv/actions
     mkdir bqcsv/actions/join
     mkdir bqcsv/actions/select
+    mkdir bqcsv/actions/colscript
     cp ../bqcsv.py bqcsv
     cp ../LICENSE bqcsv
     cp -r ../csvdb/* bqcsv/csvdb
     cp ../actions/* bqcsv/actions
     cp -r ../actions/join/* bqcsv/actions/join
     cp -r ../actions/select/* bqcsv/actions/select
+    cp -r ../actions/select/* bqcsv/actions/colscript
     find bqcsv -name "*.pyc" -exec rm \{\} \; -print
     find bqcsv -name "*.git*" -exec rm \{\} \; -print
     name=$(date +%Y%m%d)
